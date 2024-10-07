d33413d511d35679f1b58797750fbd9fd36c2da8
Sajjad Ali <sajjad.ali.9764@gmail.com>
2024-10-08 00:15:14 +0300

    Delete test3.py

--- test3.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-# Download GameStop stock data
-gamestop = yf.Ticker("GME")
-gamestop_data = gamestop.history(period="max")
-
-# Display the first few rows of the dataset
-gamestop_data.head()
